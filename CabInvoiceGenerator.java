a78c9902f11b9d699acaf14a8efaf5fd549a7932
priyankaugile <ugilepriyanka26@gmail.com>
2021-09-29 12:25:30 +0530

    Added: Calculate fare and set minimum fare.

--- CabInvoiceGenerator.java
+++
@@ -1,7 +0,0 @@
-package com.cabinvoicegenerator;
-
-public class CabInvoiceGenerator {
-    public static void main(String[] args) {
-        System.out.println("Welcome to cab invoice generator. ");
-    }
-}
